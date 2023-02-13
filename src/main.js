04dfae31f3f3d011beca287b76e35b036a0da382
danielecg <danielcastillo1408@gmail.com>
2023-02-13 00:54:11 -0300

    boton de cargar mas

--- src/main.js
+++ src/main.js
@@ -9,8 +9,30 @@ const api = axios.create({
 });
 
 //Utils
-function createMovies(movies, container) {
-    container.innerHTML= '';
+
+const lazyLoader = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+    if (entry.isIntersecting){
+    const url = entry.target.getAttribute('data-img')
+    entry.target.setAttribute('src',url);
+    }
+    })
+});
+
+
+function createMovies(
+    movies, 
+    container, 
+    {
+        lazyLoad= false, 
+        clean = true
+    } ={}
+    )
+    {
+if (clean) {
+    container.innerHTML = '';
+}
+
 
     movies.forEach(movie => {
 
@@ -24,8 +46,20 @@ function createMovies(movies, container) {
         const movieImg = document.createElement('img');
         movieImg.classList.add('movie-img');
         movieImg.setAttribute('alt', movie.title);
-        movieImg.setAttribute('src', 'https://image.tmdb.org/t/p/w300/' + movie.poster_path,);
-
+        movieImg.setAttribute(
+            lazyLoad ? 'data-img' : 'src',
+            'https://image.tmdb.org/t/p/w300/' + movie.poster_path,);
+            
+            movieImg.addEventListener('error', () => {
+            movieImg.setAttribute(
+                'src', 
+                'https:static.platzi.com/static/images/error/img404.png',
+                );
+            })
+    
+        if (lazyLoad) {
+            lazyLoader.observe(movieImg);
+        }
         movieContainer.appendChild(movieImg);        
         container.appendChild(movieContainer);
     });}
@@ -43,7 +77,7 @@ function createMovies(movies, container) {
         categoryTitle.classList.add('category-title');
         categoryTitle.setAttribute('id', 'id' + category.id);
         categoryTitle.addEventListener('click',() => {
-            location.hash = `#category=${category.id}-${category}`;
+            location.hash = `#category=${category.id}-${category.name}`;
         })
         const categoryTitleText = document.createTextNode(category.name)
         
@@ -60,7 +94,7 @@ async function getTrendingMoviesPreview(){
     const {data} = await api('trending/movie/day');
     const movies = data.results;
 
-    createMovies(movies, trendingMoviesPreviewList);
+    createMovies(movies, trendingMoviesPreviewList, true);
 }
 
 
@@ -99,9 +133,35 @@ async function getTrendingMovies(){
     const movies = data.results;
     console.log(movies);
 
-    createMovies(movies, genericSection);
+    createMovies(movies, genericSection,  { lazyLoad: true, clean: true});
+
+    const btnLoadMore = document.createElement('button')
+    btnLoadMore.innerText = 'Cargar mas';
+    btnLoadMore.addEventListener('click', getPaginatedTrendingMovies);
+    genericSection.appendChild(btnLoadMore);
 }
-async function getMovieById(id){
+
+let page = 1;
+
+async function getPaginatedTrendingMovies(){
+    page++;
+    const {data} = await api('trending/movie/day', {
+    params : {
+        page,
+    }
+    });
+    const movies = data.results;
+
+    createMovies(movies, genericSection,
+    { lazyLoad: true, clean: false});
+
+    const btnLoadMore = document.createElement('button')
+    btnLoadMore.innerText = 'Cargar mas';
+    btnLoadMore.addEventListener('click', getPaginatedTrendingMovies);
+    genericSection.appendChild(btnLoadMore);
+}
+
+async function getMovieById(id){ 
     const {data : movie} = await api('movie/' + id);
 
     const movieImgUrl = 'https://image.tmdb.org/t/p/w500/' + movie.poster_path
@@ -122,7 +182,7 @@ async function getMovieById(id){
 
     createCategories(movie.genres, movieDetailCategoriesList);
 
-    async function getRelatedMoviesId(id)
+    getRelatedMoviesId(id)
 
 }
 
